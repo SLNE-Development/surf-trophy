abe5760817ce5c8677f9328775eceec0ce55720e
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-01-10 17:05:30 +0100

    feat: enhance trophy display with dynamic lore and formatting on join
    - Added `displayName` and `buildLore` builders to customize off-hand trophy item appearance.
    - Included trophy description and received date in lore with localized formatting.

--- surf-trophy-paper/src/main/kotlin/dev/slne/surf/trophy/paper/listener/PlayerConnectionListener.kt
+++ surf-trophy-paper/src/main/kotlin/dev/slne/surf/trophy/paper/listener/PlayerConnectionListener.kt
@@ -1,12 +1,19 @@
 package dev.slne.surf.trophy.paper.listener
 
 import com.github.shynixn.mccoroutine.folia.launch
+import dev.slne.surf.surfapi.bukkit.api.builder.buildLore
+import dev.slne.surf.surfapi.bukkit.api.builder.displayName
+import dev.slne.surf.surfapi.core.api.font.toSmallCaps
+import dev.slne.surf.surfapi.core.api.util.dateTimeFormatter
 import dev.slne.surf.trophy.core.service.trophyPlayerService
 import dev.slne.surf.trophy.paper.plugin
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 object PlayerConnectionListener : Listener {
     @EventHandler
@@ -18,7 +25,41 @@ object PlayerConnectionListener : Listener {
             )
             trophyPlayerService.cachePlayer(player)
 
-            event.player.inventory.setItemInOffHand(player.selectedTrophy?.trophy?.item)
+            player.selectedTrophy?.let {
+                event.player.inventory.setItemInOffHand(it.trophy.item.apply {
+                    displayName {
+                        variableValue(it.trophy.name)
+                    }
+
+                    buildLore {
+                        emptyLine()
+                        line {
+                            variableValue("Beschreibung:".toSmallCaps())
+                        }
+
+                        line {
+                            note(it.trophy.description)
+                        }
+
+                        emptyLine()
+
+                        line {
+                            variableValue("Erhalten am:".toSmallCaps())
+                        }
+
+                        line {
+                            note(
+                                dateTimeFormatter.format(
+                                    ZonedDateTime.ofInstant(
+                                        Instant.ofEpochMilli(it.receivedAt),
+                                        ZoneId.of("Europe/Berlin")
+                                    )
+                                )
+                            )
+                        }
+                    }
+                })
+            }
         }
     }
 
